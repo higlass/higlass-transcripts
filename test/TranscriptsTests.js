c190f0742b5b24c365b71a28e117a41fe6644e05
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2020-07-23 17:46:28 -0400

    Fix test

--- test/TranscriptsTests.js
+++ test/TranscriptsTests.js
@@ -38,34 +38,42 @@ describe("Transcripts", () => {
 
     it("tests that basic infos are correct", (done) => {
 
+      
+
       const trackObj = getTrackObjectFromHGC(
         hgc.instance(),
         viewConf.views[0].uid,
         viewConf.views[0].tracks.top[0].uid
       );
 
-      expect(trackObj.trackHeight).to.equal(106);
-      expect(Object.keys(trackObj.transcriptInfo).length).to.equal(5);
-
-      const transcriptInfo = trackObj.transcriptInfo["ENST00000270722.9_chr1_3069211_3438621"];
-      expect(transcriptInfo.transcriptName).to.equal("PRDM16-201");
-      expect(transcriptInfo.txStart).to.equal(3069210);
-      expect(transcriptInfo.displayOrder).to.equal(4);
-
-      // Check position info
-      const posInfo = trackObj.transcriptPositionInfo["4"][0]
-      expect(trackObj.areTranscriptsHidden).to.equal(false);
-
-      expect(posInfo[1]).to.equal(3438621);
-
-      // Check vertical positioning of exon
       const tile = trackObj.visibleAndFetchedTiles()[2];
-      const exonPos1 = tile.allExonsForSVG[4].rect[1];
-      const exonPos2 = tile.allExonsForSVG[4].rect[5];
-      expect(exonPos1).to.equal(41);
-      expect(exonPos2).to.equal(47);
 
-      done();
+      setTimeout(() => {
+
+        expect(trackObj.trackHeight).to.equal(106);
+        expect(Object.keys(trackObj.transcriptInfo).length).to.equal(5);
+
+        const transcriptInfo = trackObj.transcriptInfo["ENST00000270722.9_chr1_3069211_3438621"];
+        expect(transcriptInfo.transcriptName).to.equal("PRDM16-201");
+        expect(transcriptInfo.txStart).to.equal(3069210);
+        expect(transcriptInfo.displayOrder).to.equal(4);
+
+        // Check position info
+        const posInfo = trackObj.transcriptPositionInfo["4"][0]
+        expect(trackObj.areTranscriptsHidden).to.equal(false);
+
+        expect(posInfo[1]).to.equal(3438621);
+
+        // Check vertical positioning of exon
+        
+        const exonPos1 = tile.allExonsForSVG[4].rect[1];
+        const exonPos2 = tile.allExonsForSVG[4].rect[5];
+        expect(exonPos1).to.equal(41);
+        expect(exonPos2).to.equal(47);
+
+        done();
+
+      }, 1000);
 
 
       
